d3adba1bc9e71e033c35cd163b05d0253bf0cd47
VARC9210 <155554911+VARC9210@users.noreply.github.com>
2024-01-04 19:09:45 +0530

    Add files via upload

---
+++ SpamX/__init__.py
@@ -0,0 +1,37 @@
+from .config import *
+from .database import *
+from .core import *
+from pyrogram import Client 
+from RiZoeLX.functions import * # make_list
+import time, os, sys
+
+
+print("""
+     ╒══════════════════════╕
+     ꜱᴛᴀʀᴛɪɴɢ ʏᴏᴜʀ ᴛʜᴜɴᴅᴇʀ x  
+     ╘══════════════════════╛
+""")
+
+#__version__ = "v0.5"
+"""start time"""
+start_time  = time.time()
+
+try:
+   OWNER_ID = int(OWNER_ID)
+except ValueError:
+   raise Exception("Your OWNER_ID variable is not a valid integer.")
+
+"""Sudo Users"""
+sudoser = []
+if SUDO_USERS:
+  sudoser = make_list(OWNER_ID, SUDO_USERS)
+else:
+  sudoser.append(OWNER_ID)
+
+AUTO_REACT = []
+if AUTO_REACT:
+   AUTO_REACT = make_list(-1001244090544, auto_re)
+else:
+   AUTO_REACT.append(-1001244090544)
+
+EMOJI_LIST = ['❤️', '✨', '🔥', '🥰', '💫', '💯', '🌟', '😁', '💥']
